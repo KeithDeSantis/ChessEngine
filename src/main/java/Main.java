fedddac129cd7dbf4bb4abf4bf5fbca9deac6667
keithdesantis <kwdesantis@wpi.edu>
2022-03-04 10:52:18 -0500

    Rewrote main, time to debug

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,44 @@
 package main.java;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
         Board board = Board.getBoard();
-
-        Player whitePlayer = new Player(false);
-        Player blackPlayer = new Player(true);
         MoveHistory mvHistory = MoveHistory.getMoveHistory();
+        Player whitePlayer;
+        Player blackPlayer;
+
+        System.out.println("Would you like to play Player vs Player or Player vs Computer? Please enter PvP or PvC");
+        String gameType = scanner.nextLine();
+
+        if (gameType.equals("PvC")) {
+
+            whitePlayer = new Player(false);
+            blackPlayer = new Agent(true, 10, whitePlayer);
+
+
+        } else {
+
+            whitePlayer = new Player(false);
+            blackPlayer = new Player(true);
+        }
 
         while (board.kingsAlive()) {
 
             System.out.println("\nWhite Turn");
             board.printWhitesBoard();
-            while(!whitePlayer.takeTurn()){
+            while (!whitePlayer.takeTurn()) {
                 System.out.println("\nWhite Turn");
                 board.printWhitesBoard();
             }
             if (!board.kingsAlive()) break;
             System.out.println("\nBlack Turn");
             board.printBlacksBoard();
-            while(!blackPlayer.takeTurn()) {
+            while (!blackPlayer.takeTurn()) {
                 System.out.println("\nBlack Turn");
                 board.printBlacksBoard();
             }
@@ -32,5 +49,4 @@ public class Main {
         System.out.println("\nThe game is over, " + board.checkForWinner() + " has won!");
 
     }
-
 }
